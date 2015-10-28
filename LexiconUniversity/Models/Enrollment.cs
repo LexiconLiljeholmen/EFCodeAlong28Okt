28f7dcc80e356a29434b1e6793c0741509a7c2a8
Adrian Lozano <adrian@xenotype.com>
2015-10-28 14:24:46 +0100

    Add refs to Student and Course from Enrollment

--- LexiconUniversity/Models/Enrollment.cs
+++ LexiconUniversity/Models/Enrollment.cs
@@ -5,7 +5,13 @@ using System.Web;
 
 namespace LexiconUniversity.Models {
     public class Enrollment {
+        public int Id { get; set; }
         public Grade Grade { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
     }
 
     public enum Grade {
